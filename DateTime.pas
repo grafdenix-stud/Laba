8af132ae5326386d1e2e2f64add4bf8bcdd8f798
Denis Chavturov <chavturov@sfedu.ru>
2022-10-06 11:31:11 +0300

    Task 5

--- DateTime.pas
+++ DateTime.pas
@@ -11,4 +11,14 @@ else if M2 > M1 then Println(D2, M2)
     
 if isv then Println(366)
 else Println(365);
+
+var (year1,year2):=ReadInteger2('Введите 2 года:');
+var sum:=0;
+if year1 > year2 then Swap(year1,year2);
+for var i:=year1 to year2 do
+  if ((year.divs(4)) and not (year.divs(100))) or (year.divs(400)) then
+    sum+=366
+  else
+    sum+=365;
+Println(sum);
 end.
